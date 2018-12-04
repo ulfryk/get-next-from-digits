622a52d166589ab856608a6a9fbbb79e6d1d6d1b
Jakub Strojewski <jakub.strojewski@gmail.com>
2018-12-04 20:04:32 +0100

    Little refactor

--- src/NextFromDigits.hs
+++ src/NextFromDigits.hs
@@ -1,25 +1,10 @@
 module NextFromDigits
-    ( getNextFromDigits
-    ) where
+  ( getNextFromDigits
+  ) where
 
 import Data.Maybe
+
 import NextFromDigits.Digits
 
-toDigit :: Char -> Int
-toDigit d = read [d]
-
-getDigits :: Int -> [Int]
-getDigits n = map toDigit . show $ n
-
-getNewDigits :: [Int] -> Digits
-getNewDigits [] = Empty
-getNewDigits (n:[]) = Pending [n]
-getNewDigits ns = foldl processD Empty . reverse $ ns
-
-getNewNum :: Digits -> Maybe String 
-getNewNum Empty = Nothing
-getNewNum (Pending _) = Nothing
-getNewNum (Updated ns) = Just (ns >>= show)
-
 getNextFromDigits :: Int -> Maybe Int
-getNextFromDigits a = (fmap (read :: String -> Int)) . getNewNum . getNewDigits . getDigits $ a
+getNextFromDigits = toNum . nextIntDigits
--- src/NextFromDigits/Digits.hs
+++ src/NextFromDigits/Digits.hs
@@ -1,7 +1,11 @@
 module NextFromDigits.Digits
-  ( Digits (Empty, Pending, Updated)
-  , processD
+  ( Digits
+  , nextIntDigits
+  , toNum
   ) where
+
+import Data.Maybe
+import Text.Read
 
 data Digits
   = Empty
@@ -14,4 +18,19 @@ processD (Updated ks) n = Updated (n:ks)
 processD (Pending (k:ks)) n
   | k > n = Updated (k:n:ks)
   | otherwise = Pending (n:k:ks)
+ 
+getDigits :: Int -> [Int]
+getDigits = map (\d -> read [d]) . show
+ 
+getNewDigits :: [Int] -> Digits
+getNewDigits [] = Empty
+getNewDigits (d:[]) = Pending [d]
+getNewDigits ds = foldl processD Empty . reverse $ ds
+ 
+toNum :: Digits -> Maybe Int
+toNum Empty = Nothing
+toNum (Pending _) = Nothing
+toNum (Updated ns) = readMaybe $ ns >>= show
 
+nextIntDigits :: Int -> Digits
+nextIntDigits = getNewDigits . getDigits
