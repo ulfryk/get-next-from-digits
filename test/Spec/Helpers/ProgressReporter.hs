761e270a9bb2458048c8068f567bbb50a30be784
Jakub Strojewski <jakub.strojewski@gmail.com>
2018-12-14 12:09:48 +0100

    Progress reporter (bar)

--- test/Spec/Helpers/ProgressReporter.hs
+++ test/Spec/Helpers/ProgressReporter.hs
@@ -1,15 +1,36 @@
 module Spec.Helpers.ProgressReporter (runTestTTReport) where
 
 import Control.Concurrent
+import qualified System.Console.Terminal.Size as S
 import System.IO
 import Test.HUnit.Base (Test, testCaseCount)
 import Test.HUnit.Text
 import Text.Printf
 
+data Bar = Bar { len :: Int, cur :: Int, tot :: Int }
+
+percent :: Bar -> Int
+percent Bar { cur = c, tot = t } = (c * 100) `div` t
+
+doneCount :: Bar -> Int
+doneCount Bar { len = l, cur = c, tot = t } = (c * l) `div` t
+
+barT :: Bar -> String
+barT b = (take dc $ repeat '=') ++ (take uc $ repeat '_')
+  where
+    dc = doneCount b
+    uc = len b - dc
+
+winWidth :: Maybe (S.Window Int) -> Int
+winWidth (Just (S.Window _ w)) = w
+winWidth _ = 50
+
 reportMsg :: Test -> String -> Bool -> Int -> IO Int
 reportMsg t _ _ l = do
-  printf "\r%03d / %03d (%03d%%)" l (testCaseCount t) (div (l * 100) (testCaseCount t))
-  (hFlush stdout >> threadDelay 100000)
+  s <- S.size
+  let theBar = Bar ((winWidth s) - 14) l (testCaseCount t)
+  printf ("\r%03d/%03d " ++ (barT theBar) ++ "[%3d%%]") (cur theBar) (tot theBar) (percent theBar)
+  (hFlush stdout >> threadDelay 350000)
   return (l + 1)
 
 runTestTTReport :: Test -> IO ()
