c11b6106eab6c3201c6eacd557b482c5a9011d85
Jakub Strojewski <jakub.strojewski@gmail.com>
2018-12-17 12:11:27 +0100

    Full custom reporter

--- test/Spec.hs
+++ test/Spec.hs
@@ -1,16 +1,25 @@
 import Data.Maybe
 import Test.HUnit.Base
 
+import Spec.Helpers.CustomReporter
 import Spec.Helpers.ProgressReporter
 import Spec.NextFromDigits
 import Spec.NextFromDigits.Digits
 
+specs :: String -> Test
+specs info = test $ info ~:
+    [ test_NextFromDigits
+    , test_digits
+    , test_toNum
+    , test_next
+    , test_identity
+    , test_identity'
+    ]
+
 main :: IO ()
-main = runTestTTReport . test $ "Spec " ~:
-  [ test_NextFromDigits
-  , test_digits
-  , test_toNum
-  , test_next
-  , test_identity
-  , test_identity'
-  ]
+main = do
+    runTestTTReport $ specs "Bar specs"
+    (cc, c) <- runTestC 0 $ specs "Full custom specs"
+    putStrLn $ show cc
+    putStrLn $ show c
+    return ()
